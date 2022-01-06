4d869869a46654d1ab7400b838b530f4317a0308
melohan <melodie.ohan@cpnv.ch>
2022-01-06 02:20:30 +0100

    Add MongoCRUD class with methods allowing CRUD operations

---
+++ Database/MongoCRUD.cs
@@ -0,0 +1,66 @@
+﻿/**
+ * Author : IAmTimCorey
+ * Sources: https://www.youtube.com/watch?v=69WBy4MHYUw
+ * Date   : 06/01/2021
+ **/
+
+using System;
+using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+/**
+ * Connect to an existing MongoDB database and provide CRUD operations
+ */
+namespace NotesCook.Database
+{
+    class MongoCRUD
+    {
+        private IMongoDatabase db;
+
+        public MongoCRUD(string database)
+        {
+            var client = new MongoClient();
+            db = client.GetDatabase(database);
+        }
+
+        public void InsertRecord<T>(string table, T record)
+        {
+            var collection = db.GetCollection<T>(table);
+            collection.InsertOne(record);
+        }
+
+        public List<T> LoadRecords<T>(string table)
+        {
+            var collection = db.GetCollection<T>(table);
+            return collection.Find(new BsonDocument()).ToList();
+        }
+
+        public T LoadRecordById<T>(string table, Guid id)
+        {
+            var collection = db.GetCollection<T>(table);
+            var filter = Builders<T>.Filter.Eq("Id", id);
+            return collection.Find(filter).First();
+        }
+
+        public void UpsertRecords<T>(string table, Guid id, T record)
+        {
+            var collection = db.GetCollection<T>(table);
+
+            var result = collection.ReplaceOne(
+                new BsonDocument("_id", id),
+                record,
+                new ReplaceOptions { IsUpsert = true}
+                );
+
+        }
+
+         public void DeleteRecords<T>(string table, Guid id)
+        {
+            var collection = db.GetCollection<T>(table);
+            var filter = Builders<T>.Filter.Eq("Id", id);
+            collection.DeleteOne(filter);
+        }
+
+    }
+}
