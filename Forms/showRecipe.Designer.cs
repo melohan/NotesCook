123e14d4e4708f5bbdf05ce9f6934cfd379901c7
TGACPNV <78166079+TGACPNV@users.noreply.github.com>
2022-01-11 20:33:38 +0100

    fix broken references

--- Forms/showRecipe.Designer.cs
+++ Forms/showRecipe.Designer.cs
@@ -29,12 +29,105 @@ namespace NotesCook.Forms
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pic_logo1 = new System.Windows.Forms.PictureBox();
+            this.lbl_title = new System.Windows.Forms.Label();
+            this.lblSteps = new System.Windows.Forms.Label();
+            this.lblIngredients = new System.Windows.Forms.Label();
+            this.lblRecipe = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_logo1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pic_logo1
+            // 
+            this.pic_logo1.Location = new System.Drawing.Point(28, 16);
+            this.pic_logo1.Name = "pic_logo1";
+            this.pic_logo1.Size = new System.Drawing.Size(45, 48);
+            this.pic_logo1.TabIndex = 9;
+            this.pic_logo1.TabStop = false;
+            // 
+            // lbl_title
+            // 
+            this.lbl_title.AutoSize = true;
+            this.lbl_title.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_title.Location = new System.Drawing.Point(123, 17);
+            this.lbl_title.Name = "lbl_title";
+            this.lbl_title.Size = new System.Drawing.Size(126, 46);
+            this.lbl_title.TabIndex = 8;
+            this.lbl_title.Text = "!Cook";
+            // 
+            // lblSteps
+            // 
+            this.lblSteps.AutoSize = true;
+            this.lblSteps.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSteps.Location = new System.Drawing.Point(192, 14);
+            this.lblSteps.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblSteps.Name = "lblSteps";
+            this.lblSteps.Size = new System.Drawing.Size(58, 17);
+            this.lblSteps.TabIndex = 8;
+            this.lblSteps.Text = "Etapes";
+            // 
+            // lblIngredients
+            // 
+            this.lblIngredients.AutoSize = true;
+            this.lblIngredients.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIngredients.Location = new System.Drawing.Point(97, 14);
+            this.lblIngredients.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblIngredients.Name = "lblIngredients";
+            this.lblIngredients.Size = new System.Drawing.Size(89, 17);
+            this.lblIngredients.TabIndex = 7;
+            this.lblIngredients.Text = "Ingrédients";
+            // 
+            // lblRecipe
+            // 
+            this.lblRecipe.AutoSize = true;
+            this.lblRecipe.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecipe.Location = new System.Drawing.Point(12, 14);
+            this.lblRecipe.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblRecipe.Name = "lblRecipe";
+            this.lblRecipe.Size = new System.Drawing.Size(72, 17);
+            this.lblRecipe.TabIndex = 6;
+            this.lblRecipe.Text = "Recettes";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblSteps);
+            this.groupBox1.Controls.Add(this.lblIngredients);
+            this.groupBox1.Controls.Add(this.lblRecipe);
+            this.groupBox1.Location = new System.Drawing.Point(9, 99);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(259, 40);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(277, 552);
+            this.Controls.Add(this.pic_logo1);
+            this.Controls.Add(this.lbl_title);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pic_logo1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pic_logo1;
+        private System.Windows.Forms.Label lbl_title;
+        private System.Windows.Forms.Label lblSteps;
+        private System.Windows.Forms.Label lblIngredients;
+        private System.Windows.Forms.Label lblRecipe;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
