a155df236a8ede10461f18239b6763307d81a792
TGACPNV <78166079+TGACPNV@users.noreply.github.com>
2022-01-19 10:13:29 +0100

    Create Show Recipe view

---
+++ Forms/ShowRecipeList.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Specialized;
+using NotesCook.Models;
+
+
+namespace NotesCook.Forms
+{
+    public partial class ShowRecipeList : Form
+    {
+        private int page;
+        private List<Recipe> recipes;
+        private OrderedDictionary groups;
+        private OrderedDictionary labels;
+        public ShowRecipeList()
+        {
+            this.recipes = Recipe.All<Recipe>();
+            this.page = 1;
+            InitializeComponent();
+            this.groups = new OrderedDictionary();
+            this.labels = new OrderedDictionary();
+            this.ListElements();
+        }
+
+        private void ShowRecipeList_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ListElements()
+        {
+            this.groups.Add("grp1",grpRecipe1);
+            this.groups.Add("grp2", grpRecipe2);
+            this.groups.Add("grp3", grpRecipe3);
+            this.groups.Add("grp4", grpRecipe4);
+
+            this.labels.Add("lbl11", lblTag11);
+            this.labels.Add("lbl12", lblTag12);
+            this.labels.Add("lbl13", lblTag13);
+            this.labels.Add("lbl14", lblTag14);
+            this.labels.Add("lbl21", lblTag21);
+            this.labels.Add("lbl22", lblTag22);
+            this.labels.Add("lbl23", lblTag23);
+            this.labels.Add("lbl24", lblTag24);
+            this.labels.Add("lbl31", lblTag31);
+            this.labels.Add("lbl32", lblTag32);
+            this.labels.Add("lbl33", lblTag33);
+            this.labels.Add("lbl34", lblTag34);
+            this.labels.Add("lbl41", lblTag41);
+            this.labels.Add("lbl42", lblTag42);
+            this.labels.Add("lbl43", lblTag43);
+            this.labels.Add("lbl44", lblTag44);
+
+        }
+
+        private void LoadPage()
+        {
+            // Hide everything
+            grpRecipe1.Visible = false;
+            lblTag11.Visible = false;
+            lblTag12.Visible = false;
+            lblTag13.Visible = false;
+            lblTag14.Visible = false;
+
+            grpRecipe2.Visible = false;
+            lblTag21.Visible = false;
+            lblTag22.Visible = false;
+            lblTag23.Visible = false;
+            lblTag24.Visible = false;
+
+            grpRecipe3.Visible = false;
+            lblTag31.Visible = false;
+            lblTag32.Visible = false;
+            lblTag33.Visible = false;
+            lblTag34.Visible = false;
+
+            grpRecipe4.Visible = false;
+            lblTag41.Visible = false;
+            lblTag42.Visible = false;
+            lblTag43.Visible = false;
+            lblTag44.Visible = false;
+
+            btnNext.Enabled = false;
+            btnPrevious.Enabled = false;
+
+            for (int i = 0; i <= 3; i++)
+            {
+                if (this.recipes.Count >= ((4 * this.page) - (3-i)))
+                {
+                    ((GroupBox)groups["grp" + (i+1).ToString()]).Text = this.recipes[(4 * this.page) - 3 - 1].Name;
+
+                    ((GroupBox)groups["grp" + (i + 1).ToString()]).Visible = true;
+
+                   for (int y = 0; y <= 3 && y+1 <= recipes[((4 * this.page) - (3 - i) - 1)].Tags.Count(); y++)
+                    {
+                        ((Label)labels["lbl" + (i + 1) + (y + 1)]).Text = recipes[((4 * this.page) - (3 - i) - 1)].Tags[y].Name;
+                        ((Label)labels["lbl" + (i + 1) + (y + 1)]).Visible = true;
+                    }
+
+                }
+            }
+
+            
+        }
+    }
+}
